39c7435bfbd8362995a6f9071c0f86911beaa66b
rosspod612 <rosspod612@tamu.edu>
2020-08-29 14:19:08 -0500

    Made PlayerController and Guard work together

---
+++ Assets/Scripts/Looker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looker : MonoBehaviour
+{
+    public GameObject guard;
+    private float reset = 5;
+    private bool movingDown;
+
+    /* Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingDown == false)
+            transform.position -= new Vector3(0, 0, 0.1f);
+        else
+            transform.position += new Vector3(0, 0, 0.1f);
+        if (transform.position.z > 10)
+            movingDown = false;
+        else if (transform.position.z < -10)
+            movingDown = true;
+        reset -= Time.deltaTime;
+        if (reset < 0)
+        {
+            guard.GetComponent<Guard>().enabled = false;
+            GetComponent<SphereCollider>().enabled = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            guard.GetComponent<Guard>().enabled = true;
+            reset = 5;
+            GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+}
