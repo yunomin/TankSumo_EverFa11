757202593d6169fd9908d925405d01dcd257be85
Yuno <yunominovo@gmail.com>
2020-08-31 21:16:07 -0500

    Add control info and completed end scene

---
+++ Assets/SourceCode/SwitchPanelMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SwitchPanelMenu : MonoBehaviour
+{
+    public GameObject BackgroundImage;
+    public GameObject BeigeColor;
+
+  
+    void Start()
+    {
+        BackgroundImage = GameObject.Find("BackgroundImage");
+        BeigeColor = GameObject.Find("BeigeColor");
+    }
+
+    public void hideControl()
+    {
+
+        BackgroundImage.SetActive(true);
+        BeigeColor.SetActive(true);
+    }
+
+    public void showControl()
+    {
+        BackgroundImage.SetActive(false);
+        BeigeColor.SetActive(false);
+    }
+}
